593a547059141ed7ba191c13285a9080873550b5
Will Gundy <willgunderson503@gmail.com>
2022-04-25 15:13:19 -0700

    added images, fixed pisces data

--- src/ZodiacCard/ZodiacCard.js
+++ src/ZodiacCard/ZodiacCard.js
@@ -5,7 +5,8 @@ export default function ZodiacCard({ zodiac }) {
   return (
     <div className='zodiac-card'>
       <p className='name'>{zodiac.name}</p>
-      <p className='name'>{zodiac.dates}</p>
+      <p className='date'>{zodiac.dates}</p>
+      <img src= {`images/${zodiac.name}.png`}/>
     </div>
   );
 }
