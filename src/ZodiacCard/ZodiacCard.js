e8c98fe7e9158f4e98527cd5111c9e0ac40d62e2
Will Gundy <willgunderson503@gmail.com>
2022-04-25 15:00:55 -0700

    added zodiac cards

--- src/ZodiacCard/ZodiacCard.js
+++ src/ZodiacCard/ZodiacCard.js
@@ -1,5 +1,11 @@
 import './ZodiacCard.css';
 
-export default function ZodiacCard() {
+export default function ZodiacCard({ zodiac }) {
   {/* in this component,  use the `name` and `dates` props to render the name and dates of the sign, as well as load the correct image  from the public directory */}
+  return (
+    <div className='zodiac-card'>
+      <p className='name'>{zodiac.name}</p>
+      <p className='name'>{zodiac.dates}</p>
+    </div>
+  );
 }
